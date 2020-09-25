cc181aa9d8cd427acadad017da2f2f0a680f7919
Carlos Jordán <calujord@gmail.com>
2020-09-25 17:02:12 +0200

    ecommerce - FILES

--- serializer/anubisoft_billings.py
+++ serializer/anubisoft_billings.py
@@ -59,7 +59,7 @@ class AnubisoftBillingsSerializer(APIJsonEncode):
                 descuento=o.get_discount_product_value(),
                 ice=0,
                 iva=o.get_taxes(),
-                total=o.get_total_service_price(),
+                total=o.get_total_service_with_discount(),
                 codigoIVA="2" if o.get_taxes() > 0 else "0",
                 fechaExpiracion=None, lote=None, serie=None, producto=True
             )
